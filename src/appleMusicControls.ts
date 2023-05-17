9ca25af3e11191d22250e47ba52761b2bf9534d1
skitscha <steven.kitscha@gmail.com>
2023-05-16 23:26:41 -0500

    Update appleMusicControls.ts

--- src/appleMusicControls.ts
+++ src/appleMusicControls.ts
@@ -32,7 +32,7 @@ const setTrackProgress = (trackProgress: number) => {
     const lcdRoot = document.querySelector('amp-lcd').shadowRoot;
     const playbackProgressInput = lcdRoot.querySelector('#playback-progress') as HTMLInputElement;
     const maxLength = Number.parseInt(playbackProgressInput.max);
-    if(trackProgress <= maxLength){
+    if (trackProgress <= maxLength) {
         playbackProgressInput.value = trackProgress.toString();
         playbackProgressInput.dispatchEvent(new Event('change'));
     }
@@ -43,22 +43,45 @@ const setVolume = (volume: number) => {
     const volumeControlInputRoot = musicPlayerRoot.querySelector('amp-volume-control').shadowRoot;
     const volumeControlInput = volumeControlInputRoot.querySelector('input') as HTMLInputElement;
     const maxVolume = Number.parseInt(volumeControlInput.max);
-    if(volume <= maxVolume){
+    if (volume <= maxVolume) {
         volumeControlInput.value = volume.toString();
         volumeControlInput.dispatchEvent(new Event('change'));
     }
 }
 
-const queueSong = (albumId:number, songId: number ) => {
-    const url = new URL(`https://music.apple.com/us/album/album/${albumId}?i=${songId}`);
-    history.pushState({}, "", url);
-    history.back();
+const docReady = (fn: any) => {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
 }
 
-
-const playnextSong = () => {
-    history.forward();
+const selectedSongContainerObserver = new MutationObserver(() => {
     const selectedSongContainer = document.querySelector('.songs-list-row--selected');
-    const playButton = selectedSongContainer.querySelector('.play-button') as HTMLButtonElement;
-    playButton.click();
+    if (selectedSongContainer) {
+        const playButton = selectedSongContainer.querySelector('.play-button') as HTMLButtonElement;
+        playButton.click();
+        selectedSongContainerObserver.disconnect();
+        history.back();
+    }
+});
+
+const playNextSong = (albumId: number, songId: number) => {
+    const url = new URL(`https://music.apple.com/us/album/album/${albumId}?i=${songId}`);
+    history.pushState({}, "", url);
+    window.dispatchEvent(
+        new PopStateEvent("popstate", {
+          state: history.state
+        }
+    ));
+    docReady(() => {
+        selectedSongContainerObserver.observe(document.body, {
+            childList: true,
+            subtree: true
+        })
+    });
 }
