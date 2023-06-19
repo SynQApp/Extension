25df6f7a07be3e5615dea163be1214a807dc0095
Mitchell K <mkossoris@gmail.com>
2023-06-19 11:09:31 -0700

    Implemented SpotifyController (#5)

--- src/contents/spotify.ts
+++ src/contents/spotify.ts
@@ -1,14 +1,19 @@
 import type { PlasmoCSConfig } from 'plasmo';
 
+import { SpotifyController } from '~lib/controllers/SpotifyController';
+import { registerControllerHandler } from '~lib/message-handlers/registerControllerHandler';
 import { onDocumentReady } from '~util/onDocumentReady';
 
 export const config: PlasmoCSConfig = {
-  matches: ['*://*.spotify.com/*'],
+  matches: ['*://open.spotify.com/*'],
   all_frames: true
 };
 
 const initialize = () => {
   console.info('SynQ: Initializing Spotify');
+
+  const controller = new SpotifyController();
+  registerControllerHandler(controller);
 };
 
 onDocumentReady(initialize);
