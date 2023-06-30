e9e611236ec7bf056d8fe30aed70fbd0a5e39756
Mitchell Kossoris <mkossoris@gmail.com>
2023-06-29 23:57:11 -0500

    Remove unused imports

--- src/popup/screens/Controller/index.tsx
+++ src/popup/screens/Controller/index.tsx
@@ -1,6 +1,3 @@
-import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-
 import useControllerScreen from './useControllerScreen';
 
 const ControllerScreen = () => {
