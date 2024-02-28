97f089300dcfd409d6acdf5d5255ca4e524f3f36
Mitchell Kossoris <mkossoris@gmail.com>
2024-02-27 17:08:19 -0800

    Update apple music link

--- src/ui/onboard/screens/Complete.tsx
+++ src/ui/onboard/screens/Complete.tsx
@@ -19,7 +19,7 @@ import { Screen } from '../components/Screen';
 const EXAMPLE_SPOTIFY_LINK =
   'https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC';
 const EXAMPLE_APPLE_MUSIC_LINK =
-  'https://music.apple.com/us/album/never-gonna-give-you-up/1558533900?i=1558534271';
+  'https://music.apple.com/us/album/never-gonna-give-you-up/1452434638?i=1452434833';
 const EXAMPLE_YOUTUBE_MUSIC_LINK =
   'https://music.youtube.com/watch?v=lYBUbBu4W08';
 
