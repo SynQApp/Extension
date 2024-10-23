d7906d626a9d108fb94b4f5855648882f525c01e
Mitchell Kossoris <mkossoris@gmail.com>
2024-10-22 23:43:34 -0700

    Update package version and add GitHub notice on settings page

--- src/ui/options/components/OptionsContent.tsx
+++ src/ui/options/components/OptionsContent.tsx
@@ -10,6 +10,23 @@ import { PreferredServiceLinksSection } from './PreferredServiceLinksSection';
 export const OptionsContent = () => {
   return (
     <Container>
+      <GitHubContainer>
+        <GitHubTitle type="subtitle" size="lg" weight="semibold">
+          We're open source!
+        </GitHubTitle>
+        <Text type="body" size="md">
+          Please star our GitHub repo if you like this extension, and if you're
+          a programmer, feel free to contribute to the code!
+        </Text>
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=SynQApp&repo=Extension&type=star&count=true&size=large"
+          frameBorder="0"
+          scrolling="0"
+          width="130"
+          height="30"
+          title="GitHub"
+        ></iframe>
+      </GitHubContainer>
       <Title type="display" size="4xl">
         Settings
       </Title>
@@ -30,4 +47,15 @@ const Container = styled.div`
 
 const Title = styled(Text)`
   font-weight: ${token('typography.fontWeights.bold')};
+`;
+
+const GitHubContainer = styled.div`
+  background-color: rgb(255 255 255 / 7%);
+  border: 1px solid #ffffff;
+  border-radius: 4px;
+  padding: 12px;
+`;
+
+const GitHubTitle = styled(Text)`
+  margin: 0;
 `;
